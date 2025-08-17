b24429169885f73aa45bf7ee471a489e6e986a09
mohammadah20 <ahmadi.moghaddam.1384@gmail.com>
2025-08-17 17:34:51 +0330

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 
-Square = ({value}) =>{
+Square = () =>{
 
   const [value,setValue]=useState(null);
   
